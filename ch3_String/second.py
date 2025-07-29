ea6c7599c041a380828f5a2257fbd55e6d230b31
darshit3596 <sdr3596@gmail.com>
2025-07-29 16:35:35 +0530

    pythone learning

---
+++ ch3_String/second.py
@@ -0,0 +1,7 @@
+name = "darshit"
+print(name.capitalize())  # Capitalizes the first letter
+print(name.upper())       # Converts all letters to uppercase
+print(name.lower())       # Converts all letters to lowercase
+print(name.title())       # Capitalizes the first letter of each word
+print(name.swapcase())    # Swaps the case of each letter  DaRsHiT      small
+print(name.replace("d", "k"))  # Replaces 'd' with 'D           
---
+++ ch3_String/thir.py
@@ -0,0 +1,21 @@
+name = "darshit" 
+
+print(name[0:3])
+
+print(name[3:])  # Prints from index 3 to the end = shit
+
+print(name[1:5])  # Prints from index 1 to index 4  =arsh
+
+print(name[:3])  # Prints every second character = dar 
+
+print(name[3:])  # Prints every second character = shit
+
+print(name[::2])  # Prints every second character = drs
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[-6])
+print(name[-7])
