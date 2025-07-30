ebc9da12cbd46037b231db90a30e606b14053353
darshit3596 <sdr3596@gmail.com>
2025-07-30 17:44:30 +0530

    30/07/2025 work

---
+++ ch2_opretors/Arithmetic_ops.py
@@ -0,0 +1,19 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+print (a+b)  # This will concatenate the strings
+
+print (int(a)+int(b))  # This will add the numbers as integers   type=1 
+
+print (float(a)+float(b))  # This will add the numbers as floats  type=2 
+
+c =int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print (c+b)  
+print (F"subtract the numbers == {c-b}")  # This will subtract the numbers
+print (F"multiply the numbers == {c*b}")  # This will multiply the numbers   
+print (F"divide the numbers == {c/b}")  # This will divide the numbers
+print (F"remainder of the division == {c%b}")  # This will get the remainder of the division
+print (F"power of the second number == {c**b}")  # This will raise the first number to the power of the second number
+print (F"floor division == {c//b}")  # This will perform floor division
+print (F"square the first number == {c**2}")  # This will square the first number
+  
---
+++ ch2_opretors/Userinput.py
@@ -0,0 +1,6 @@
+Fname = input("Enter the file name: ")
+designation = input("Enter your designation: ")
+
+
+print(F"""Hi, I’m {Fname}, a passionate learner with a background in {designation}. I enjoy building creative projects, solving problems, and continuously improving my skills. I’m hardworking, adaptable, and always eager to learn and contribute in a team environment.
+""")
